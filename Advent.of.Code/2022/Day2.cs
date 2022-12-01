415c986c8122b88ad4b2a9995d45283a4cb09e95
Michael Harris <michael.harris@impower.ai>
2022-12-01 10:12:47 -0500

    Added day 2

---
+++ Advent.of.Code/2022/Day2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Advent.of.Code._2022
+{
+    class Day2
+    {
+        public void Run()
+        {
+
+            #region Init
+            var textLines = File.ReadAllLines("C:\\Users\\MichaelHarris\\OneDrive - Agility Partners\\Documents\\repos\\Internal\\Advent\\Advent.of.Code\\Advent.of.Code\\Data\\day_1_input.txt");
+            // Part 1
+            List<int> elves = new List<int>();
+            int max = 0;
+            int calories = 0;
+
+            // Part 2
+            int count = 0;
+            int multipleElves = 0;
+
+            #endregion
+
+            #region Part1
+            foreach (var line in textLines)
+            {
+                // is line numeric
+                if(int.TryParse(line, out int num))
+                {
+                    calories += num;
+
+                }
+                else
+                {
+                    elves.Add(calories);
+                    // set max value
+                    if(max < calories) { max = calories; }
+                    calories = 0;
+                }
+            }
+
+            Console.WriteLine("Max number is: " + max);
+            #endregion
+
+            #region Part2
+            do
+            {
+                multipleElves += elves.Max();
+                elves.Remove(elves.Max());
+                count++;
+
+            } while (count < 3);
+            Console.WriteLine("Calories of top 3 elves are: " + multipleElves);
+            #endregion
+        }
+    }
+}
--- Advent.of.Code/Program.cs
+++ Advent.of.Code/Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Advent.of.Code._2022;
 
-Console.WriteLine("Hello, World!");
-
-Day1 Day1 = new Day1();
-Day1.Run();
+Day2 Day2 = new Day2();
+Day2.Run();
 
 
 
