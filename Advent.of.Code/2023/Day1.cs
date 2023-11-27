5495bd30edc1b91b2def1d004798b35e3be16ec0
Michael Harris <michael.harris@impower.ai>
2023-11-27 10:19:15 -0500

    init: Ready for advent of code 2023

---
+++ Advent.of.Code/2023/Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.of.Code._2023
+{
+    public static class Day1
+    {
+        public static void Run(string[] input)
+        {
+            Console.WriteLine("READY FOR ADVENT 2023!");
+        }
+    }
+}
--- Advent.of.Code/Program.cs
+++ Advent.of.Code/Program.cs
@@ -1,16 +1,78 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Advent.of.Code._2022;
 using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
-var textLines = File.ReadAllLines("C:\\Users\\MichaelHarris\\OneDrive - Agility Partners\\Documents\\repos\\Internal\\Advent\\Advent.of.Code\\Advent.of.Code\\Data\\2022\\day_10_input.txt");
+class Program
+{
+    static string Year = "2023";
+    static string Day = "1";
+    
+    static void Main()
+    {
+        string[] input = ReadText(Year,Day);
+        RunDay(Year,Day,input);
+    }
 
 
-var input = File.ReadAllText("C:\\Users\\MichaelHarris\\OneDrive - Agility Partners\\Documents\\repos\\Internal\\Advent\\Advent.of.Code\\Advent.of.Code\\Data\\2022\\day_10_input.txt")
-            .Split(' ', '\r', '\n').
-            Where(x => x != "").ToArray();
+    static string[] ReadText(string year, string day)
+    {
+        string solutionDirectory = GetSolutionDirectory();
+        string directoryPath = Path.Combine(solutionDirectory, "Data", year);
+        string filePath = Path.Combine(directoryPath, $"day_{day}_input.txt");
+        var textlines = File.ReadAllLines(filePath);
+        return File.ReadAllText("C:\\Users\\MichaelHarris\\OneDrive - Agility Partners\\Documents\\repos\\Internal\\Advent\\Advent.of.Code\\Advent.of.Code\\Data\\2022\\day_10_input.txt")
+               .Split(' ', '\r', '\n').
+               Where(x => x != "").ToArray();
+    }
 
+    static string GetSolutionDirectory()
+    {
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
-Day10.Run(input);
+        while (directory != null && directory.Name != "Advent.of.Code")
+        {
+            directory = directory.Parent;
+        }
+
+        return directory?.FullName;
+    }
+
+    static void RunDay(string year, string day, string[] input)
+    {
+        string className = $"Day{day}";
+        Type type = Type.GetType($"Advent.of.Code._{year}.{className}"); ;
+        try
+        {
+            if (type != null)
+            {
+                MethodInfo methodInfo = type.GetMethod("Run");
+                if (methodInfo != null)
+                {
+                    // Define input parameters
+                    object[] parameters = { input }; 
+
+                    // Invoke the method, since it is a static class no instance is needed.
+                    methodInfo.Invoke(null, parameters);
+                }
+                else
+                {
+                    Console.WriteLine($"Method 'Run' not found in class {className}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Class {className} not found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
 
 
 
